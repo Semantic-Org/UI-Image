cefc914cb07ff8b09901b4adc1a6fb2e3cf8289e
Pusher Robot <jack@semantic-ui.com>
2015-07-17 16:49:40 -0400

    Updated component to version 2.0.4

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
   name    : 'semantic:ui-image',
   summary : 'Semantic UI - Image: Single component release',
-  version : '2.0.3',
+  version : '2.0.4',
   git     : 'git://github.com/Semantic-Org/UI-Image.git',
 });
 
